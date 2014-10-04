7c3429d3b4fef5c38cc9f029de029fd6714d58e9
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-04 12:25:33 -0700

    Finish testing PersistenceEngine

---
+++ PynamoDB/persistence_engine.py
@@ -0,0 +1,24 @@
+"""
+    persistence_engine.py
+    ~~~~~~~~~~~~
+    Implements put, get, delete methods for PersistenceStage.  Using an actual persistence engine (i.e. MySQL, BDB), one would implement the three methods themselves.
+    """
+
+
+class PersistenceEngine(object):
+    """ Basic persistence engine implemented as a regular Python dict."""
+
+    def __init__(self):
+        self._persistence = {}
+
+    def put(self, key, value):
+        """ Put key value pair into storage"""
+        self._persistence[key] = value
+
+    def get(self, key):
+        """ Get key's value """
+        return self._persistence[key]
+
+    def delete(self, key):
+        """ Delete key value pair """
+        del self._persistence[key]
---
+++ PynamoDB/test_persistence_engine.py
@@ -0,0 +1,49 @@
+"""
+    test_persistence_engine.py
+    ~~~~~~~~~~~~
+    Tests PersistenceEngine's put, get, delete methods.  In this case they're a regular Python dict, but for the sake of completeness, I'll include a few tests here.
+
+    Run tests with:
+    clear; python -m unittest discover -v
+"""
+
+import unittest
+from persistence_engine import PersistenceEngine
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.p = PersistenceEngine()
+        self.key = "key"
+        self.value = "value"
+
+    def tearDown(self):
+        pass
+
+    def test_put(self):
+        """ Tests put command """
+        try:
+            self.p.put(self.key, self.value)
+        except:
+            self.fail()
+
+    def test_get(self):
+        """ Tests get command """
+        try:
+            self.p.put(self.key, self.value)
+            value = self.p.get(self.key)
+            self.assertEqual(value, self.value)
+        except:
+            self.fail()
+
+    def test_delete(self):
+        """ Tests delete command """
+        try:
+            self.p.put(self.key, self.value)
+            self.p.delete(self.key)
+        except:
+            self.fail()
+
+        with self.assertRaises(KeyError) as error:
+            value = self.p.get(self.key)
