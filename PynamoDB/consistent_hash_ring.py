0d6f69a6a77ddce57cd2b3cfbb2dc37a949b3924
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-05 13:28:47 -0700

    Add methods for consistent hash ring

---
+++ PynamoDB/consistent_hash_ring.py
@@ -0,0 +1,37 @@
+import bisect
+
+class ConsistentHashRing(object):
+    """
+        Implements a consistent hash ring, that given a list of nodes, determines the n nodes responsible for holding a given key.
+
+        It is assumed that nodes and keys have been hashed appropriately prior to interaction with hash ring.
+    """
+
+    def __init__(self, node_hashes=None):
+        self._hash_ring = sorted(node_hashes)
+
+    def __len__(self):
+        """ Returns the number of nodes still in hash ring"""
+        return len(self._hash_ring)
+
+
+    def add_node_hash(self, node_hash=None):
+        if node_hash:
+            self._node_hash.insort(self._hash_ring, node_hash)
+
+    def remove_node_hash(self, node_hash=None):
+        if node_hash:
+            self._node_hash.remove(node_hash)
+
+    def get_responsible_node_hashes(self, key_hash=None, num_replicas=3):
+        """ Returns:
+                n nodes clockwise of given key hash where n = num_replicas
+
+            Example:
+                for nodes [ 'A', 'B', 'C', 'D' ] and key 'AA', [ 'B', 'C', 'D' ] would be returned.
+        """
+
+        if key_hash:
+            primary_position = bisect.bisect_left(self._hash_ring, key_hash)
+            return [self._hash_ring[ i % len(self._hash_ring) ] for i in xrange(primary_position, primary_position + num_replicas)]
+
