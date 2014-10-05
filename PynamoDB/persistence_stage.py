2b9b6d7a39ab993ffcd748b8c779fb30eb303952
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-05 02:00:34 -0700

    Finish testing PersistenceStage

---
+++ PynamoDB/persistence_stage.py
@@ -0,0 +1,60 @@
+from error_code import ErrorCode
+from persistence_engine import PersistenceEngine
+
+class PersistenceStage(object):
+    """ Stage for managing key-value persistence."""
+
+    def __init__(self):
+        self._persistence_engine = PersistenceEngine()
+
+    def put(self, key, value):
+        """ Returns:
+                error code \x00 if put is successful
+                error code \x06 if unknown error is encountered
+
+            Compares timestamps of old and new values if key is already present in hash ring
+        """
+
+        try:
+            old_timestamp = self._persistence_engine.get(key).timestamp
+        except KeyError:
+            self._persistence_engine.put(key, value)
+        except:
+            return ErrorCode('\x06')
+        else:
+            if value.timestamp > old_timestamp:
+                self._persistence_engine.put(key, value)
+
+        return ErrorCode('\x00')
+
+    def get(self, key):
+        """ Returns:
+                (error code \x00, value) if get is successful
+                (error code \x01, None) if key inexistant
+                (error code \x06, None) if unknown error is encountered
+        """
+
+        try:
+            value = self._persistence_engine.get(key)
+        except KeyError:
+            return (ErrorCode('\x01'), None)
+        except:
+            return (ErrorCode('\x06'), None)
+        else:
+            return (ErrorCode('\x00'), value)
+
+    def delete(self, key):
+        """ Returns:
+                error code \x00 if delete is successful
+                error code \x01 if key inexistant
+                error code \x06 if unknown error is encountered
+        """
+
+        try:
+            self._persistence_engine.delete(key)
+        except KeyError:
+            return ErrorCode('\x01')
+        except:
+            return ErrorCode('\x06')
+        else:
+            return ErrorCode('\x00')
---
+++ PynamoDB/test_persistence_stage.py
@@ -0,0 +1,97 @@
+"""
+    test_persistence_stage.py
+    ~~~~~~~~~~~~
+    Tests that PersistenceEngine's put, get, delete methods raise the correct error codes.
+
+    Run tests with:
+    clear; python -m unittest discover -v
+"""
+
+import unittest
+from persistence_stage import PersistenceStage
+from error_code import ErrorCode
+import util
+
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.ps = PersistenceStage()
+        self.key = "key"
+        self.value = util.TimestampedValue("value")
+
+    def tearDown(self):
+        pass
+
+    def test_put_success(self):
+        """ Tests put command """
+        try:
+            error_code = self.ps.put(self.key, self.value)
+            self.assertEqual(error_code, ErrorCode('\x00'))
+        except:
+            self.fail()
+
+    def test_put_new_value(self):
+        """ Tests put command """
+        try:
+            old_value = util.TimestampedValue("new_value")
+            new_value = util.TimestampedValue("new_value")
+
+            error_code = self.ps.put(self.key, old_value)
+            self.assertEqual(error_code, ErrorCode('\x00'))
+            error_code = self.ps.put(self.key, new_value)
+            self.assertEqual(error_code, ErrorCode('\x00'))
+
+            error_code, value = self.ps.get(self.key)
+            self.assertEqual(value, new_value)
+            self.assertEqual(error_code, ErrorCode('\x00'))
+        except:
+            self.fail()
+
+    def test_put_old_value(self):
+        """ Tests put command """
+        try:
+            error_code = self.ps.put(self.key, self.value)
+            self.assertEqual(error_code, ErrorCode('\x00'))
+        except:
+            self.fail()
+
+    def test_get_success(self):
+        """ Tests get command """
+        try:
+            self.ps.put(self.key, self.value)
+            error_code, value = self.ps.get(self.key)
+            self.assertEqual(value, self.value)
+            self.assertEqual(error_code, ErrorCode('\x00'))
+        except:
+            self.fail()
+
+    def test_get_inexistant_key(self):
+        """ Tests get command on inexistant key"""
+        try:
+            self.ps.get(self.key)
+            error_code, value = self.ps.get(self.key)
+            self.assertEqual(value, None)
+            self.assertEqual(error_code, ErrorCode('\x01'))
+        except:
+            self.fail()
+
+    def test_delete_success(self):
+        """ Tests delete command """
+        try:
+            self.ps.put(self.key, self.value)
+            error_code = self.ps.delete(self.key)
+            error_code, value = self.ps.get(self.key)
+            self.assertEqual(error_code, ErrorCode('\x01'))
+        except:
+            self.fail()
+
+    def test_delete_inexistant_key(self):
+        """ Tests delete command on inexistant key"""
+        try:
+            error_code = self.ps.delete(self.key)
+            self.assertEqual(error_code, ErrorCode('\x01'))
+        except:
+            self.fail()
+
+
