b9e2fb2345ba17d6b63b610c2b036dfe964c652d
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-05 13:21:44 -0700

    Add test template

---
+++ PynamoDB/test_template.py
@@ -0,0 +1,17 @@
+"""
+    .py
+    ~~~~~~~~~~~~
+    clear; python -m unittest discover -v
+"""
+
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test(self):
+        pass
