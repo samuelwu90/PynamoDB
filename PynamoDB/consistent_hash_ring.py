679037113dba6e439b60bed0474c523e70629b41
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-09 03:20:23 -0700

    Add some logging and docstrings

--- PynamoDB/consistent_hash_ring.py
+++ PynamoDB/consistent_hash_ring.py
@@ -1,13 +1,16 @@
 import bisect
+import logging
 
 class ConsistentHashRing(object):
-    """
-        Implements a consistent hash ring, that given a list of nodes, determines the n nodes responsible for holding a given key.
+    """ Implements a consistent hash ring, that given a list of nodes, determines the n nodes responsible for holding a given key.
 
         It is assumed that nodes and keys have been hashed appropriately prior to interaction with hash ring.
     """
 
     def __init__(self, node_hashes=None):
+        self.logger = logging.getLogger('{}'.format(self.__class__.__name__))
+        self.logger.debug('__init__')
+
         if node_hashes:
             self._hash_ring = sorted(node_hashes)
         else:
@@ -19,10 +22,12 @@ class ConsistentHashRing(object):
 
 
     def add_node_hash(self, node_hash=None):
+        """ add node hash to hash ring """
         if node_hash:
             bisect.insort(self._hash_ring, node_hash)
 
     def remove_node_hash(self, node_hash=None):
+        """ remove node from hash ring """
         if node_hash:
             self._hash_ring.remove(node_hash)
 
@@ -34,7 +39,8 @@ class ConsistentHashRing(object):
                 for nodes [ 'A', 'B', 'C', 'D' ] and key 'AA', [ 'B', 'C', 'D' ] would be returned.
         """
 
+        self.logger.info('get_responsible_node_hashes')
+        self.logger.info('get_responsible_node_hashes.  key_hash, num_replicas: {}, {}'.format(key_hash, num_replicas))
         if key_hash:
             primary_position = bisect.bisect_left(self._hash_ring, key_hash)
             return [self._hash_ring[ i % len(self._hash_ring) ] for i in xrange(primary_position, primary_position + num_replicas)]
-
