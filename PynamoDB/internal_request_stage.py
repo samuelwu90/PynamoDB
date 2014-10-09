55cdaef0e6092030606cb3e342a4c68fe4d52b8d
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-09 10:24:42 -0700

    Add InternalChannel
    Async I/O channel that handles connections with other nodes.  Acts as
    an endpoint for received internal communications - if a request is
    received it is handled right away.   Not sure if this is the best way
    to do things, but creating an InternalRequest object seems redundant
    since it would just process the message and push a reply back.

--- PynamoDB/internal_request_stage.py
+++ PynamoDB/internal_request_stage.py
@@ -51,4 +51,75 @@ class InternalRequestStage(asyncore.dispatcher):
     def _immediate_shutdown(self):
         self.handle_close()
 
+class InternalChannel(asynchat.async_chat):
+    """ Handles receiving requests from other nodes """
+    def __init__(self, server=None, sock=None, client_address=None, node_hash=None, coordinator_listener=None):
+        self.logger = logging.getLogger('{}'.format(self.__class__.__name__))
+        self.logger.debug('__init__')
+
+        self._server = server
+
+        self.set_terminator(self._server.terminator)
+
+        if sock:                # for incoming communication
+            self._client_address = client_address
+            asynchat.async_chat.__init__(self, sock)
+
+        elif node_hash:     # for outgoing communication
+            self._coordinator_listener = coordinator_listener
+            node_address = self._server.membership_stage.node_address(node_hash)
+            hostname, port = node_address.split(":")
+            self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self.connect((hostname, port))
+            except:
+                self.logger.error('__init__.  failed to connect' )
+            asynchat.async_chat.__init__(self)
+
+    def collect_incoming_data(self, data):
+        self._read_buffer.append(data)
+
+    def found_terminator(self):
+        message = json.loads(''.join(self._read_buffer))
+        self._process_incoming_message(message)
+        self.close_when_done()
+
+    def _send_message(self, message):
+        self.push(util.pack_message(message, self._server._terminator))
+
+    def _process_incoming_message(self, message):
+        self.logger.debug('_handle_message.  type: {}'.format(message['type']))
+        if messsage['type'] == 'request':
+            if request['command'] == 'put':
+                reply = self._server.persistence_stage.put(messsage['key'], messsage['value'], messsage['timestamp'])
+            elif request['command'] == 'get':
+                reply  = self._server.persistence_stage.get(messsage['key'])
+            elif request['command'] == 'delete':
+                reply = self._server.persistence_stage.delete(messsage['key'])
+            self.send_message(reply)
+        elif message['type'] == 'reply':
+            self._coordinator_listener.send(message)
+        elif message['type'] == 'announced failure':
+            self._handle_announced_failure_message(message)
+        elif message['type'] == 'unannounced failure':
+            self._handle_unannounced_failure_message(message)
+
+    def _handle_announced_failure_message(self, message):
+        self.logger.debug('_handle_announced_failure')
+        # change membership
+        # accept any keys
+        # propagate message
+            #exponential backoff
         pass
+
+    def _handle_unannounced_failure_message(self, message):
+        self.logger.debug('_handle_unannounced_failure')
+        # change membership
+        # accept any keys
+        # repartition keys if one of n + 2 replicas affected.
+            # pass keys out
+        pass
+
+    def handle_error(self):
+        self.logger.error('handle_error')
+
