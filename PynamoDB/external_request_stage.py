f8bb0c616c7a320064180bb81c4bffbd249737db
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-09 03:28:59 -0700

    Add ExternalChannel
    Async channel that maintains a connection with client.  Creates
    ExternalRequest objects upon each received requests and sends replies
    back to client in the proper order.

--- PynamoDB/external_request_stage.py
+++ PynamoDB/external_request_stage.py
@@ -51,4 +51,54 @@ class ExternalRequestStage(asyncore.dispatcher):
         self.logger.debug('_immediate_shutdown')
         self.handle_close()
 
+class ExternalChannel(asynchat.async_chat):
+    """ Channel that handles communication with with client."""
+    def __init__(self, server=None, sock=None):
+        self.logger = logging.getLogger('{}'.format(self.__class__.__name__))
+        self.logger.debug('__init__')
+
+        asynchat.async_chat.__init__(self, sock)
+        self._server = server
+        self._read_buffer = list()
+        self._request_queue = list()
+
+
+        self.set_terminator(self._server.terminator)
+
+    def collect_incoming_data(self, data):
+        self.logger.debug('collect_incoming_data.  collected data: {}'.format(data))
+        self._read_buffer.append(data)
+
+    def found_terminator(self):
+        request = json.loads(''.join(self._read_buffer))
+        self._handle_request(request)
+
+        self.logger.debug('found_terminator.  request: {}'.format(request))
+
+    def _send_message(self, message):
+        self.push(util.pack_message(message, self._server._terminator))
+
+    def _handle_request(self, request):
+        self.logger.debug('_handle_request.')
+        request['timestamp'] = util.current_time()
+        request['key'] = util.get_hash(request['key'])
+        external_request = ExternalRequest(request=request, server=self._server)
+        self._request_queue.append(external_request)
+    def handle_error(self):
         pass
+
+    def process(self):
+        self.logger.info('process')
+        if not self._request_queue:
+            self.handle_close()
+
+        for external_request in self._request_queue:
+            external_request.process()
+
+        for external_request in self._request_queue:
+            if external_request.completed:
+                self._send_message(external_request._reply)
+                self._request_queue.pop(0)
+            else:
+                break
+
