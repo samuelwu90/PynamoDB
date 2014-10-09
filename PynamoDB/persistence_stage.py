1847768e17536399f70ba0e2fc15fccff758d9e1
SAMUEL WU <samuelwu90@users.noreply.github.com>
2014-10-09 03:17:52 -0700

    Add replies to put, get, delete.
    Requests now flow into the system as json dicts and pass through until
    it reaches an endpoint, the PersistenceStage being one of them.
    
    It now returns a reply dict to be processed by the
    InternalRequestCoordinator.

--- PynamoDB/persistence_stage.py
+++ PynamoDB/persistence_stage.py
@@ -1,16 +1,34 @@
-from util import ErrorCode
+import logging
+import util
 from persistence_engine import PersistenceEngine
 
 class PersistenceStage(object):
     """ Stage for managing key-value persistence."""
 
-    def __init__(self):
+    def __init__(self, server=None):
+        self.logger = logging.getLogger('{}'.format(self.__class__.__name__))
+        self.logger.debug('__init__')
+
+        self._server = server
         self._persistence_engine = PersistenceEngine()
 
     def keys(self):
         return self._persistence_engine.keys()
 
-    def put(self, key, value):
+    def partition_keys(self):
+        """ Returns:
+                a dict() where:
+                    keys: node_hashes
+                    values: keys for which the given node_hash is responsible
+        """
+        partitioned_keys = collections.defaultdict(list)
+
+        for key in self.server.persistence_stage.keys():
+            partitioned_keys[key.node_hash].append(key)
+
+        return partitioned_keys
+
+    def put(self, key, value, timestamp=None):
         """ Returns:
                 error code \x00 if put is successful
                 error code \x06 if unknown error is encountered
@@ -18,17 +36,32 @@ class PersistenceStage(object):
             Compares timestamps of old and new values if key is already present in hash ring
         """
 
-        try:
-            old_timestamp = self._persistence_engine.get(key).timestamp
-        except KeyError:
-            self._persistence_engine.put(key, value)
-        except:
-            return ErrorCode('\x06')
-        else:
-            if value.timestamp > old_timestamp:
-                self._persistence_engine.put(key, value)
+        self.logger.debug('put')
 
-        return ErrorCode('\x00')
+        reply = {
+                        'node_hash' : self._server.node_hash,
+                        'error_code' : None
+                        }
+        try:
+            if not timestamp:
+                new_timestamp = util.get_timestamp()
+            else:
+                new_timestamp = timestamp
+            try:
+                old_timestamp = self._persistence_engine.get(key)['timestamp']
+            except KeyError:
+                self._persistence_engine.put(key, value, new_timestamp)
+                reply['error_code'] = '\x00'
+            except:
+                reply['error_code'] = '\x06'
+            else:
+                if new_timestamp > old_timestamp:
+                    self._persistence_engine.put(key, value, new_timestamp)
+                    reply['error_code'] = '\x00'
+            finally:
+                return reply
+        except Exception as e:
+            print e, sys.exc_info()
 
     def get(self, key):
         """ Returns:
@@ -37,14 +70,25 @@ class PersistenceStage(object):
                 (error code \x06, None) if unknown error is encountered
         """
 
+        self.logger.debug('get')
+
+        reply = {
+                        'node_hash' : self._server.node_hash,
+                        'error_code' :  None,
+                        'value' : None,
+                        'timestamp': None
+                        }
+
         try:
-            value = self._persistence_engine.get(key)
+            value, timestamp = self._persistence_engine.get(key)
         except KeyError:
-            return (ErrorCode('\x01'), None)
+            reply['error_code'] = '\x01'
         except:
-            return (ErrorCode('\x06'), None)
+            reply['error_code'] = '\x06'
         else:
-            return (ErrorCode('\x00'), value)
+            reply['error_code'] = '\x00'
+        finally:
+            return reply
 
     def delete(self, key):
         """ Returns:
@@ -53,11 +97,20 @@ class PersistenceStage(object):
                 error code \x06 if unknown error is encountered
         """
 
+        self.logger.debug('delete')
+
+        reply = {
+                        'node_hash' : self._server.node_hash,
+                        'error_code' : None
+                        }
         try:
             self._persistence_engine.delete(key)
         except KeyError:
-            return ErrorCode('\x01')
+            reply['error_code'] = '\x01'
         except:
-            return ErrorCode('\x06')
+            reply['error_code'] = '\x06'
         else:
-            return ErrorCode('\x00')
+            reply['error_code'] = '\x00'
+        finally:
+            return reply
+
